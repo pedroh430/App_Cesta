c4c831a9fccb0e702230a3e916c668f4dc7e0310
pedro <pedrobldap41@gmail.com>
2026-03-23 22:49:45 -0300

    ok

---
+++ Frontend/js/main.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click",() => {
+        const target = tab.getAttribute("data-tab");
+
+        tabs.forEach(t => t.classList.remove("active"));
+        contents.forEach(c => c.classList.remove("active"));
+
+        tab.classList.add("active");
+        document.getElementById(target).classList.add("active");
+    })
+})
